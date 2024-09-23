90c9876af7a0aa2235e8c28fcc9c2033194cc753
zzy4086371517 <166327859+zzy4086371517@users.noreply.github.com>
2024-09-22 22:37:12 -0500

    Update app1.py

--- src/app1.py
+++ src/app1.py
@@ -2,18 +2,18 @@
 
 from flask import Flask, request
 
-app = Flask(__name__)
-
-@app.route("/")
-def main():
-    return '''
+def create_app():
+    app1 = Flask(__name__)
+    @app1.route("/")
+    def main():
+        return '''
      <form action="/echo_user_input" method="POST">
          <input name="user_input">
          <input type="submit" value="Submit!">
      </form>
      '''
-
-@app.route("/echo_user_input", methods=["POST"])
-def echo_input():
-    input_text = request.form.get("user_input", "")
-    return "You entered: " + input_text
+    @app1.route("/echo_user_input", methods=["POST"])
+    def echo_input():
+        input_text = request.form.get("user_input", "")
+        return "You entered: " + input_text
+app1 = create_app()
