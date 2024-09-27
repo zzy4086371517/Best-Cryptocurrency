563030a063984fe55a3aab7e56cc70df763d0aa3
Zhanyi Zuo <zhanyizuo@MacBook-Pro-2.attlocal.net>
2024-09-26 22:20:56 -0500

    fixed app1.py

--- src/app1.py
+++ src/app1.py
@@ -2,18 +2,17 @@
 
 from flask import Flask, request
 
-def create_app():
-    app1 = Flask(__name__)
-    @app1.route("/")
-    def main():
-        return '''
+
+app1 = Flask("BestCrypto")
+@app1.route("/")
+def main():
+    return '''
      <form action="/echo_user_input" method="POST">
          <input name="user_input">
          <input type="submit" value="Submit!">
      </form>
      '''
-    @app1.route("/echo_user_input", methods=["POST"])
-    def echo_input():
-        input_text = request.form.get("user_input", "")
-        return "You entered: " + input_text
-app1 = create_app()
+@app1.route("/echo_user_input", methods=["POST"])
+def echo_input():
+    input_text = request.form.get("user_input", "")
+    return "You entered: " + input_text
